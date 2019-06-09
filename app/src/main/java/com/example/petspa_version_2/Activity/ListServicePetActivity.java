70f8426d4c6db6b3988ed45a602a7950540d17e3
LongDB <nhacsisitinh@gmail.com>
2019-06-09 20:48:05 +0700

    - xóa các dòng code dư thừa

--- app/src/main/java/com/example/petspa_version_2/Activity/ListServicePetActivity.java
+++ app/src/main/java/com/example/petspa_version_2/Activity/ListServicePetActivity.java
@@ -87,18 +87,6 @@ public class ListServicePetActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == ValueGoalball.REQUEST_CODE) {
-            if (resultCode == ValueGoalball.RESULT_CODE_BACK_HOME) {
-                Intent intent = ListServicePetActivity.this.getIntent();
-                ListServicePetActivity.this.setResult(ValueGoalball.RESULT_CODE_BACK_HOME, intent);
-                finish();
-            }
-        }
-    }
-
-    @Override
     public void onAttachFragment(@NonNull Fragment fragment) {
         super.onAttachFragment(fragment);
 
--- app/src/main/java/com/example/petspa_version_2/Goalball/ValueGoalball.java
+++ app/src/main/java/com/example/petspa_version_2/Goalball/ValueGoalball.java
@@ -10,7 +10,4 @@ public class ValueGoalball {
     public static final String CARD_VIEW_2 = "Service 2";
     public static final String CARD_VIEW_3 = "Service 3";
     public static final String CARD_VIEW_4 = "Service 4";
-
-    public static int REQUEST_CODE = 25;
-    public static int RESULT_CODE_BACK_HOME = 26;
 }
