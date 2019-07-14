58a40033e9c1e17f3d603ec090a4b4fa4eed9691
danghuule1998 <danghuule1998@gmail.com>
2019-07-14 14:29:29 +0700

    horizontal service

---
+++ app/src/main/java/com/example/petspa_version_2/Adapter/ListHorizontalServiceFragmentAdapter.java
@@ -0,0 +1,74 @@
+package com.example.petspa_version_2.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.petspa_version_2.Listener.List_Service_Card_Item_Listener;
+import com.example.petspa_version_2.Model.ServicePet;
+import com.example.petspa_version_2.R;
+
+import java.util.List;
+
+public class ListHorizontalServiceFragmentAdapter extends RecyclerView.Adapter<ListHorizontalServiceFragmentAdapter.MyViewHolder> {
+    private List<ServicePet> list;
+    private List_Service_Card_Item_Listener listener;
+
+    public ListHorizontalServiceFragmentAdapter(List<ServicePet> list, List_Service_Card_Item_Listener listener) {
+        this.list = list;
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_service_cardview, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, final int position) {
+        holder.imageServiceCardView.setImageResource(list.get(position).getServiceImage());
+        holder.txtServiceCardContent.setText(list.get(position).getServiceTitle());
+        holder.txtServiceCardPrice.setText(list.get(position).getServicePrice());
+        holder.ratingBar.setRating(list.get(position).getRating());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                ServicePet service =list.get(position);
+
+                listener.viewService(service);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+        ImageView imageServiceCardView;
+        TextView txtServiceCardContent;
+        TextView txtServiceCardPrice;
+        RatingBar ratingBar;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageServiceCardView = itemView.findViewById(R.id.imageServiceCardView);
+            txtServiceCardContent = itemView.findViewById(R.id.txtServiceCardContent);
+            txtServiceCardPrice = itemView.findViewById(R.id.txtServiceCardPrice);
+            ratingBar = itemView.findViewById(R.id.ratingBar);
+        }
+    }
+}
